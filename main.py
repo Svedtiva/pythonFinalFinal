e411c4e9f816a1b1c910e61d0628ed7aeb4798eb
Svedtiva <rileydkennedy2003@gmail.com>
2023-05-09 00:16:58 -0400

    Page redesign

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from sqlalchemy import Column, Integer, String, Numeric, create_engine, text, Table, MetaData, select, engine, and_
 
 app = Flask(__name__)
@@ -19,9 +19,19 @@ def h(str):
     return sum
 
 
+@app.route('/in')
+def re():
+    return render_template('index.html')
+
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    query = text("SELECT * FROM finalproducts")
+    result = conn.execute(query)
+    products = []
+    for row in result:
+        products.append(row)
+    return render_template('customer.html', products=products)
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -45,6 +55,7 @@ def login():
             session['email'] = user[3]
             session['full_name'] = user[4]
             session['type'] = user[5]
+            session['logged_in'] = True
             if user[5] == 'Admin':
                 acc = conn.execute(text("SELECT id FROM finalaccounts"))
                 return redirect(url_for('admin', acc=acc))
@@ -53,7 +64,7 @@ def login():
                 return redirect(url_for('vendor', acc=acc))
             else:
                 acc = conn.execute(text("SELECT id FROM finalaccounts"))
-                return redirect(url_for('viewProducts', acc=acc))
+                return redirect(url_for('customer', acc=acc))
     else:
         return render_template('index.html')
 
@@ -108,11 +119,13 @@ def add_account():
     full_name = first_name+' '+last_name
     password = h(password)
     type = 'customer'
-
-    query = text("INSERT INTO finalaccounts (id, username, password, email, full_name,"
-                 " type)"
-                 " VALUES (:id, :username, :password, :email, :full_name, :type"
-                 " )")
+    user = conn.execute(text("SELECT * FROM finalaccounts WHERE email=:email"), {'email': email}).fetchone()
+    if user is not None:
+        flash("An account with this email already exists.")
+        return redirect(url_for('register'))
+    else:
+        query = text("INSERT INTO finalaccounts (id, username, password, email, full_name,"" type)"
+                     " VALUES (:id, :username, :password, :email, :full_name, :type"" )")
     params = {"id": new_id, "username": username, "password": password, "email": email, "full_name": full_name,
               "type": type}
     conn.execute(query, params)
@@ -135,12 +148,12 @@ def add_product():
     customizations = request.form['customizations']
     stock = request.form['stock']
 
-    query = text("INSERT INTO finalproducts (id, vendor_id, title, description, image, customizations,"
-                 " price)"
-                 " VALUES (:id, :vendor_id, :description, :customizations, :image,"
-                 " :price)")
-    params = {"id": new_id, "vendor_id": vendor_id, "title": title, "description": description,
-              "price": price, "image": image, "customizations": customizations}
+    query = text("INSERT INTO finalproducts (item_id, vendor_id, title, description, image, customizations,"
+                 " price, stock)"
+                 " VALUES (:item_id, :vendor_id, :title, :description, :customizations, :image,"
+                 " :price, :stock)")
+    params = {"item_id": new_id, "vendor_id": vendor_id, "title": title, "description": description,
+              "price": price, "image": image, "customizations": customizations, 'stock': stock}
     conn.execute(query, params)
     conn.commit()
 
@@ -148,7 +161,7 @@ def add_product():
 
 
 @app.route('/vendor_add_product', methods=['POST'])
-def add_product():
+def vendor_add_product():
     max_id_query = text("SELECT MAX(item_id) FROM finalproducts")
     max_id = conn.execute(max_id_query).fetchone()[0]
     new_id = max_id + 1 if max_id is not None else 1
@@ -161,16 +174,86 @@ def add_product():
     customizations = request.form['customizations']
     stock = request.form['stock']
 
-    query = text("INSERT INTO finalproducts (id, vendor_id, title, description, image, customizations,"
-                 " price)"
-                 " VALUES (:id, :vendor_id, :description, :customizations, :image,"
-                 " :price)")
-    params = {"id": new_id, "vendor_id": vendor_id, "title": title, "description": description,
-              "price": price, "image": image, "customizations": customizations}
+    query = text("INSERT INTO finalproducts (item_id, vendor_id, title, description, image, customizations,"
+                 " price, stock)"
+                 " VALUES (:item_id, :vendor_id, :description, :customizations, :image,"
+                 " :price, :stock)")
+    params = {"item_id": new_id, "vendor_id": vendor_id, "title": title, "description": description,
+              "price": price, "image": image, "customizations": customizations, 'stock': stock}
     conn.execute(query, params)
     conn.commit()
 
     return url_for('vendor')
+
+
+@app.route('/admin_delete_product/<int:item_id>', methods=['POST'])
+def admin_delete_product(item_id):
+    query = text("DELETE FROM finalproducts WHERE item_id = :item_id")
+    conn.execute(query, {"item_id": item_id})
+    conn.commit()
+    return redirect(url_for('admin'))
+
+
+@app.route('/admin_edit_product/<int:item_id>', methods=['GET', 'POST'])
+def admin_edit_product(item_id):
+    if request.method == 'POST':
+        vendor_id = request.form['vendor_id']
+        title = request.form['title']
+        description = request.form['description']
+        price = request.form['price']
+        image = request.form['image']
+        customizations = request.form['customizations']
+        stock = request.form['stock']
+        query = text("UPDATE finalproducts SET vendor_id=:vendor_id, title=:title, description=:description, price=:price, image=:image, customizations=:customizations, stock=:stock WHERE item_id=:item_id")
+        params = {"vendor_id": vendor_id, "title": title, "description": description, "price": price, "image": image, "customizations": customizations, "stock": stock, "item_id": item_id}
+        conn.execute(query, params)
+        conn.commit()
+        flash("Product updated successfully.")
+        return redirect(url_for('admin'))
+    else:
+        query = text("SELECT * FROM finalproducts WHERE item_id=:item_id")
+        params = {"item_id": item_id}
+        result = conn.execute(query, params)
+        product = result.fetchone()
+        if product is None:
+            flash("Product not found.")
+            return redirect(url_for('admin'))
+        else:
+            return render_template('edit_product.html', product=product)
+
+
+@app.route('/vendor_edit_product/<int:item_id>', methods=['GET', 'POST'])
+def vendor_edit_product(item_id):
+    if request.method == 'POST':
+        vendor_id = request.form['vendor_id']
+        title = request.form['title']
+        description = request.form['description']
+        price = request.form['price']
+        image = request.form['image']
+        customizations = request.form['customizations']
+        stock = request.form['stock']
+        query = text("UPDATE finalproducts SET vendor_id=:vendor_id, title=:title, description=:description, price=:price, image=:image, customizations=:customizations, stock=:stock WHERE item_id=:item_id")
+        params = {"vendor_id": vendor_id, "title": title, "description": description, "price": price, "image": image, "customizations": customizations, "stock": stock, "item_id": item_id}
+        conn.execute(query, params)
+        conn.commit()
+        flash("Product updated successfully.")
+        return redirect(url_for('admin'))
+    else:
+        query = text("SELECT * FROM finalproducts WHERE item_id=:item_id")
+        params = {"item_id": item_id}
+        result = conn.execute(query, params)
+        product = result.fetchone()
+        if product is None:
+            flash("Product not found.")
+            return redirect(url_for('admin'))
+        else:
+            return render_template('edit_product.html', product=product)
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
